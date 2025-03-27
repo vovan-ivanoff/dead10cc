a02a3bcfa1263ff6b018dcccf1748d904bf35707
Vladimir <143862659+fastixr@users.noreply.github.com>
2025-03-27 20:24:13 +0300

    MAYBE FIX?

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,8 +3,10 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from api.routers import all_routers
+from api import health
 
 app = FastAPI(title="TootEvent")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://127.0.0.1:5500", "http://0.0.0.0:5500", "*"],
@@ -12,8 +14,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 for router in all_routers:
     app.include_router(router)
+
+app.include_router(health.router)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", reload=True, host="0.0.0.0")
